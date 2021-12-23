2e171040e79432e85793de6e505227e7b4c299a5
unknown <hindhujhasridhar@gmail.com>
2021-12-23 10:49:46 +0530

    GAMBLER IS WIN / LOSS

---
+++ GamblingGame/Win and Lose/WinOrLose.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace GamblingGame
+{
+    public class WinOrLose
+    {
+        public void display()
+        {
+
+            Random random = new Random();
+            int bet = random.Next(0, 2);
+
+            if (bet == 1)
+            {
+                Console.WriteLine("GAMBLER WIN 1 DOLLAR");
+            }
+            else
+            {
+                Console.WriteLine("GAMBLER LOSS 1 DOLLAR");
+            }
+        }
+    }
+}
