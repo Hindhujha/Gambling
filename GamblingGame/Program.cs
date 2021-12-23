53e821aac9a36a4d06e616674bfc851dff6843df
unknown <hindhujhasridhar@gmail.com>
2021-12-23 10:54:10 +0530

    GAMBLERS STAKE AND BET

--- GamblingGame/Program.cs
+++ GamblingGame/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System;
+namespace GamblingGame
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int stake = 100; 
+            int bet = 1;
+            Console.WriteLine("GAMBLER STAKE FOR EVERDAY :" + stake);
+            Console.WriteLine("GAMBLER BET:" + bet);
+            while (true)
+            {
+                Console.WriteLine("-----------------");
+                Console.WriteLine("1.WIN/LOSE");
+                Console.WriteLine("ENTER YOUR OPTION");
+
+                int select = Convert.ToInt32(Console.ReadLine());
+
+                switch (select)
+                {
+                    case 1:
+                        WinOrLose myObj = new WinOrLose();
+                        myObj.display();
+                        break;
+                    default:
+                        Console.WriteLine("ENTER A CORRECT OPTION");
+                        break;
+
+                }
+            }
+        }
+                    
+
+  }
+    }
+
+
